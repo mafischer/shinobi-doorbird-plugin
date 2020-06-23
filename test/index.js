f8953910a9beff4391860118c5d7f0ea6f383814
Michael Fischer <michaelandrewfischer@gmail.com>
2020-06-23 00:06:39 -0500

    adding support for shinobi 2.0.0

--- test/index.js
+++ test/index.js
@@ -1,22 +1,8 @@
-const Code = require('code');   // assertion library
-const Lab = require('lab');
-const lab = exports.lab = Lab.script();
+const {expect} = require('chai');
+const doorbird = require('../lib');
 
-//const server = require('../lib');
-//const routes = require('../lib/routes');
-
-//TODO: need to mock database
-// lab.test('server module exists', (done) => {
-//     Code.expect(server).to.exist();
-//     done();
-// });
-
-// lab.test('routes module exists', (done) => {
-//     Code.expect(routes).to.exist();
-//     done();
-// });
-
-lab.test('fake test', (done) => {
-    Code.expect(lab).to.exist();
-    done();
+describe('doorbird', () => {
+    it('the library should be defined', () => {
+        expect(doorbird).to.not.equal(undefined);
+    });
 });
